012ac837396bb7728f61a8dd5475d49af847082c
AndrewPhills <andrei97filip@gmail.com>
2021-02-07 01:25:46 +0200

    With wannabe html intended. Still with PHP ```contact_me.php``` and ```contact_me.js``` ajax

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -1,7 +1,7 @@
 <?php
 // Check for empty fields
 if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-  http_response_code(500);
+  http_response_code(500); // internal server error if it is not completed
   exit();
 }
 
@@ -11,11 +11,11 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = "yourname@yourdomain.com"; // Add your email address in between the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
-$subject = "Website Contact Form:  $name";
+$to = "andrei97filip@gmail.com"; //  replacing yourname@yourdomain.com - This is where the form will send a message to.
+$subject = "Website Contact Form:  $name"; //subject from $name
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
-$header = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$header .= "Reply-To: $email";	
+$header = "From: noreply@Andrew-Phills-PresentationPage\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$header .= "Reply-To: $email";	//replay-back to your user
 
 if(!mail($to, $subject, $body, $header))
   http_response_code(500);
